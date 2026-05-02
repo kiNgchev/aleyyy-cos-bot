e50ccdcfb7e6042828d4c9968bc4140ccc729926
KiNgchev <kingchev.net@gmail.com>
2026-05-02 09:51:19 +0300

    update all

---
+++ locales/en/LC_MESSAGES/aleybot.ftl
@@ -0,0 +1,3 @@
+Start = Hello! This is the bot for purchasing a sign. Send your receipt of purchase via [Boosty](https://boosty.to/aleyyforyou) or ⭐️ here, and write what you’d like to see on the sign. Thank you for your purchase <3
+Success = Thank for your purchase! The sign will be sent via this bot within a week.
+Memo = Memo
--- main.py
+++ main.py
@@ -2,101 +2,129 @@ import asyncio
 import csv
 import logging
 import sys
+from contextlib import suppress
 from os import getenv
 
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher, F, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message, BufferedInputFile, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, FSInputFile, CallbackQuery
+from aiogram_i18n import I18nMiddleware, I18nContext
+from aiogram_i18n.cores import FluentRuntimeCore
 
-TOKEN = getenv("BOT_TOKEN")
-ALEY_ID = getenv("ALEY_ID")
-fieldnames = [
-    "original_id", "copy_id"
-]
+TOKEN = getenv("TOKEN")
+ALEY_ID = int(getenv("ALEY_ID"))
+fieldnames = ["original_chat_id", "original_message_id", "copy_id"]
 
-dp = Dispatcher()
+router = Router()
 
-
-# TODO: Реализовать обработку выбора языка
-async def handle_language_choosing(callback_query, lang: str):
-    await callback_query.message.answer_photo(
-        caption=,
-        photo=BufferedInputFile.from_file('start_pic.png')
-    )
-
-@dp.callback_query(F.data == "lang_handler_ru")
-async def handle_language_choosing_ru(callback_query):
-    await handle_language_choosing(callback_query, 'ru')
-
-@dp.callback_query(F.data == "lang_handler_en")
-async def handle_language_choosing_en(callback_query):
-    await handle_language_choosing(callback_query, 'en')
-
-
-@dp.message(CommandStart())
+@router.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    keyboard = InlineKeyboardMarkup(inline_keyboard = [[
-        InlineKeyboardButton(text="Russian", callback_data="lang_handler_ru"),
-        InlineKeyboardButton(text="English", callback_data="lang_handler_en")
-    ]])
-    await message.answer(
-        text="Choose your language:",
-        reply_markup=keyboard
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text="English", callback_data="en")],
+            [InlineKeyboardButton(text="Русский", callback_data="ru")]
+        ]
+    )
+    await message.answer("Choose language:", reply_markup=keyboard)
+
+
+@router.callback_query(F.data == "en")
+async def handle_en_switch(callback: CallbackQuery, i18n: I18nContext):
+    await handle_lang_switch(callback, i18n, "en")
+
+
+@router.callback_query(F.data == "ru")
+async def handle_en_switch(callback: CallbackQuery, i18n: I18nContext):
+    await handle_lang_switch(callback, i18n, "ru")
+
+
+async def handle_lang_switch(callback: CallbackQuery, i18n: I18nContext, locale_code: str):
+    await i18n.set_locale(locale_code)
+    await callback.message.delete()
+    await callback.message.answer_photo(
+        caption=i18n.get('Start'),
+        photo=FSInputFile('./img/start_pic.jpg'),
+        parse_mode="Markdown",
+        reply_markup=InlineKeyboardMarkup(
+            inline_keyboard=[
+                [InlineKeyboardButton(text=i18n.get('Memo'), url='https://t.me/aleyyycosplay/1327')]
+            ]
+        )
     )
 
-@dp.message()
-async def aley_handler(message: Message) -> None:
-    if message.from_user.id != ALEY_ID:
-        return
 
+async def aley_handler(message: Message) -> None:
     if message.reply_to_message is None:
         return
 
-    original_id = ''
+    original_chat_id = None
+    original_message_id = None
+
     with open("pairs.csv", mode="r", encoding="utf-8") as file:
         reader = csv.DictReader(file, fieldnames = fieldnames)
         for row in reader:
-            if row["copy_id"] == message.reply_to_message.message_id:
-                original_id = row['original_id']
+            if row["copy_id"] == str(message.reply_to_message.message_id):
+                original_chat_id = int(row['original_chat_id'])
+                original_message_id = int(row['original_message_id'])
         file.flush()
 
-    try:
-        await message.send_copy(chat_id=original_id)
-    except TypeError:
-        await message.answer("Что-то пошло не так.")
-
-
-@dp.message()
-async def members_handler(message: Message) -> None:
-    if message.from_user.id == ALEY_ID:
+    if original_chat_id is None or original_message_id is None:
         return
+
+    try:
+        await message.send_copy(chat_id=original_chat_id, reply_to_message_id=original_message_id)
+    except TypeError:
+        await message.answer("Somthing went wrong")
+
+
+async def members_handler(message: Message, i18n: I18nContext) -> None:
     try:
         send_message = await message.send_copy(chat_id=ALEY_ID)
 
-        with open("pairs.csv", mode="w", newline="", encoding="utf-8") as file:
+        with open("pairs.csv", mode="a+", newline="", encoding="utf-8") as file:
             writer = csv.DictWriter(file, fieldnames=fieldnames)
             writer.writeheader()
 
             row = {
-                'original_id': message.message_id,
+                'original_chat_id': message.chat.id,
+                'original_message_id': message.message_id,
                 'copy_id': send_message.message_id
             }
 
             writer.writerow(row)
             file.flush()
-
+        await message.answer(i18n.get('Success'))
     except TypeError:
-        await message.answer("Что-то пошло не так.")
+        await message.answer("Somthing went wrong")
+
+
+@router.message()
+async def message_handler(message: Message, i18n: I18nContext) -> None:
+    if message.from_user.id == ALEY_ID:
+        await aley_handler(message)
+    else:
+        await members_handler(message, i18n)
 
 
 async def main() -> None:
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
+    dp.include_router(router)
 
+    i18n_middleware = I18nMiddleware(
+        core=FluentRuntimeCore(
+            path="locales/{locale}/LC_MESSAGES"
+        ),
+        default_locale='ru'
+    )
+    i18n_middleware.setup(dispatcher=dp)
+
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
     await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    with suppress(KeyboardInterrupt):
+        asyncio.run(main())
